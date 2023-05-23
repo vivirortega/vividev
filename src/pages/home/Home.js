92898a613af7bae085c99510fd0aa695774f1833
vivirortega <vitoriarortega@gmail.com>
2023-05-23 18:51:43 -0300

    feat: home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,24 @@
 import Header from "../../components/Header/Header";
 import { Main } from "./style";
+import vivi from "../../assets/vivi.png"
 
 function Home() {
   return (
     <Main>
     <Header/>
-      <h1>Sou a Home</h1>
+    <div className="info">
+      <div className="user-info">
+      <h1>alert("Hello World!");</h1>
+      <h2>Sou Vivi, Desenvolvedora Full Stack e UX/UI Designer.</h2>
+      <div className="buttons">
+      <button className="contact">Contato
+      </button>
+      <button className="cv">CV</button>
+      </div>
+      </div>
+      <img src={vivi}/>
+      </div>
+      
     </Main>
   )
 }
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -1,7 +1,58 @@
 import styled from 'styled-components'
 
 const Main = styled.body`
-  background-color: #121212;
+  background-color: #000000;
+
+  .info {
+    display: flex;
+    gap: 3em;
+    justify-content: center;
+    margin-top: 10em;
+  }
+
+  .user-info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  h1 {
+    color: #6bd425;
+  }
+
+  h2 {
+    color: #ffffff;
+  }
+
+  img {
+    width: 27em;
+    height: 28em;
+    border-radius: 10px;
+  }
+
+  .buttons {
+    display: flex;
+    gap: 1em;
+    margin-top: 5em;
+  }
+
+  .contact, .cv {
+    width: 7em;
+    height: 2em;
+    border: none;
+    border-radius: 10px;
+    color: #ffffff;
+    font-size: 1.80em;
+  }
+
+  .contact {
+    background-color: #42113c;
+  }
+
+  .cv {
+    background-color: #6bd425;
+  }
 
 
 `
