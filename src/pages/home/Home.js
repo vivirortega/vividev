71f9e712d4dabcbdc647db7f8dfde7ddf3db8833
vivirortega <vitoriarortega@gmail.com>
2023-05-23 20:09:45 -0300

    feat: download cv when button clicked

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,12 @@
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 
+import { Slide } from 'react-awesome-reveal'
+import { Link } from 'react-router-dom'
+
 import { Main } from './style'
 import vivi from '../../assets/vivi.png'
+import pdf from "../../assets/CV.pdf"
 
 function Home() {
   return (
@@ -10,13 +14,17 @@ function Home() {
       <Header />
       <div className="info">
         <div className="user-info">
-          <div className='titles'>
-          <h1>alert("Hello World!👩‍💻");</h1>
-          <h2>Desenvolvedora Full Stack e UX/UI Designer.</h2>
+          <div className="titles">
+            <h1>alert("Hello World!👩‍💻");</h1>
+            <h2>Desenvolvedora Full Stack e UX/UI Designer.</h2>
           </div>
           <div className="buttons">
-            <button className="contact">Contato</button>
-            <button className="cv">CV</button>
+            <Link to="/contact">
+              <button className="contact">Contato</button>
+            </Link>
+            <a href={pdf} download="CV.pdf">
+              <button className="cv">CV</button>
+            </a>
           </div>
         </div>
         <img src={vivi} />
